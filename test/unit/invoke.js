05d5847ce46500562a5de47977e92c96462c3944
binaryalchemist <kristopher.l.alexander@gmail.com>
2016-07-11 12:42:32 -0400

    issue-3 - making arguments a "pass through" to close Issue #3.

---
+++ test/unit/invoke.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var sinon = require('sinon');
+
+describe('invoke.js', function() {
+    var funcMock;
+    var args;
+
+    before(function() {
+        funcMock = { apply: sinon.stub() };
+        args = [{bucket: true}, 'key', {value:{}}];
+        var invoke = require('../../lib/invoke.js');
+        invoke(funcMock, args);
+    });
+
+    it('should call apply on function all args except the first one', function() {
+        var expectedArgs = ['key', {value:{}}];
+        assert.equal(funcMock.apply.calledWith(null, expectedArgs), true);
+    });
+});
