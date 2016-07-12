279abfb7a530fc093e6d50fd1f6f6a508b98cc8e
binaryalchemist <kristopher.l.alexander@gmail.com>
2016-07-12 14:38:10 -0400

    issue-3 - fixing scoping issue.

--- test/unit/invoke.js
+++ test/unit/invoke.js
@@ -9,11 +9,11 @@ describe('invoke.js', function() {
         funcMock = { apply: sinon.stub() };
         args = [{bucket: true}, 'key', {value:{}}];
         var invoke = require('../../lib/invoke.js');
-        invoke(funcMock, args);
+        invoke({}, funcMock, args);
     });
 
     it('should call apply function on all args except the first one', function() {
         var expectedArgs = ['key', {value:{}}];
-        assert.equal(funcMock.apply.calledWith(null, expectedArgs), true);
+        assert.equal(funcMock.apply.calledWith({}, expectedArgs), true);
     });
 });
